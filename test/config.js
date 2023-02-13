7158f98903427b58915cfe0e660cf24845eb58a2
SHAHARZI <shaharzi@rafael.co.il>
2023-02-13 11:00:50 +0000

    Upload New File

---
+++ test/config.js
@@ -0,0 +1,13 @@
+var AllureReporter = require('allure_report')
+exports.congfig = {
+    seleniumAddress: 'http//localhost:4444/wd/hub',
+    specs: [spec.js],
+onPrepare: function() {
+    jasmine.getEnv().addReporter(new AllureReporter{});
+        jasmine.getEnv().afterEach(function{done}) {
+            browser.takescreeshot{}.then{function (png) }
+
+        }
+
+}
+}
