ba5b2f0e0f5783822b076465c58ab4dd8d0e20e2
shahar.ziman <shahar.ziman@gmail.com>
2024-02-28 18:26:19 +0200

    change node filr

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var AllureReporter = require('allure_report')
 exports.congfig = {
-    seleniumAddress: 'http//localhost:3444/wd/hub',
+    seleniumAddress: 'http//localhost:4444/wd/hub',
     specs: [spec.js],
 onPrepare: function() {
     jasmine.getEnv().addReporter(new AllureReporter{});
